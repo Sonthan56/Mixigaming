dd8f81a4fe94501a0901a97a1dfe7679f8d815f3
than-van-son <24103351@st.phenikaa-uni.edu.vn>
2026-09-25 00:36:33 +0700

    Upload website

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mixigaming
---
+++ scriprt.js
@@ -0,0 +1,4 @@
+// Hàm thông báo thêm vợt Li-Ning vào giỏ hàng
+function addToCart(productName) {
+  alert(`Đã thêm thành công sản phẩm Li-Ning "${productName}" vào giỏ hàng của SHOP-ST!`);
+}
